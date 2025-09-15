9daa0c76504a87fe00c8b810a981051e031c86b4
IT24100151 <IT24100151@my.sliit.lk>
2025-09-15 22:06:25 +0530

    LAB07_PS

---
+++ Lab Sheet 07/IT24100151_LAB07.R
@@ -0,0 +1,15 @@
+getwd()
+setwd('C:\\Users\\ketty\\OneDrive\\Desktop\\IT24100151')
+
+# Problem 1
+punif(25, min=0, max=40) - punif(10, min=0, max=40)  
+
+# Problem 2
+pexp(2, rate = 1/3)
+
+# Problem 3.i
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+# Problem 3.ii
+qnorm(0.95, mean = 100, sd = 15)
+
